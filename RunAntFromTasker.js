a3112b9b3956e6275666bc4778ee7f46f54f2814
Lveby <917388970@qq.com>
2021-01-27 20:48:52 +0800

    V1.02.03 //从tasker指定的程序会是Tmp.js，目录在/storage/emulated/0/脚本

--- RunAntFromTasker.js
+++ RunAntFromTasker.js
@@ -1,4 +1,9 @@
-var path = engines.myEngine().cwd();
+//从tasker指定的程序会是Tmp.js，目录在/storage/emulated/0/脚本
+//从tasker指定的程序会是Tmp.js，目录在/storage/emulated/0/脚本
+//从tasker指定的程序会是Tmp.js，目录在/storage/emulated/0/脚本
+
+var path = engines.myEngine().cwd();///storage/emulated/0/脚本
+path += "/auto-js-ant-release";
 console.log("在"+path+"下，运行RunAnt.js");
 engines.execScriptFile(path+"/RunAnt.js");
 
