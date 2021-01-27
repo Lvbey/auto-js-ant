c7754c41498bf216d9bfeceadc2586c90771117e
917388970@qq.com <917388970@qq.com>
2021-01-27 15:30:30 +0800

    update download url

--- module/AntConfig.js
+++ module/AntConfig.js
@@ -4,10 +4,10 @@ AntConfig.ExescriptFileName = "Ant.js";
 AntConfig.ProjectName = "auto-js-ant";
 
 //这个地址不能变动
-AntConfig.VersionUrl = "https://gitee.com/Lvbey/open-projects-version-config/raw/master/auto-js-ant/version";
+AntConfig.VersionUrl = "https://gitee.com/Lvbey/auto-js-ant/raw/release/version";
 
 AntConfig.VersionNow = "21.01.20";
-AntConfig.DownloadUrl = "https://gitee.com/Lvbey/auto-js-ant/repository/archive/master.zip";
+AntConfig.DownloadUrl = "https://gitee.com/Lvbey/auto-js-ant/repository/archive/release.zip";
 
 
 AntConfig.StorageName = "auto-js-ant";
