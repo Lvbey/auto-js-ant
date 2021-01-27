2fc0ded83981db559cb6776809e55c12920814b4
Lveby <917388970@qq.com>
2021-01-27 20:55:40 +0800

    V1.02.05
    path {Array} | {string} 指定脚本运行的目录。这些路径会用于require时寻找模块文件。

--- RunAntFromTasker.js
+++ RunAntFromTasker.js
@@ -5,6 +5,8 @@
 var path = engines.myEngine().cwd();///storage/emulated/0/脚本
 path += "/auto-js-ant-release";
 console.log("在"+path+"下，运行RunAnt.js");
-engines.execScriptFile(path+"/RunAnt.js");
+engines.execScriptFile(path+"/RunAnt.js",{
+    path:path //这个非常重要，否则require 会找不到文件
+});
 
 exit();
