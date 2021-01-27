6af6bbd16a020315af8fab0b73651086c89d3e36
917388970@qq.com <917388970@qq.com>
2021-01-27 15:21:14 +0800

    release version files

--- module/AntConfig.js
+++ module/AntConfig.js
@@ -3,8 +3,12 @@ var AntConfig = {};
 AntConfig.ExescriptFileName = "Ant.js";
 AntConfig.ProjectName = "auto-js-ant";
 
-AntConfig.VersionNow = "21.01.20";
+//这个地址不能变动
 AntConfig.VersionUrl = "https://gitee.com/Lvbey/open-projects-version-config/raw/master/auto-js-ant/version";
+
+AntConfig.VersionNow = "21.01.20";
+AntConfig.DownloadUrl = "https://gitee.com/Lvbey/auto-js-ant/repository/archive/master.zip";
+
 
 AntConfig.StorageName = "auto-js-ant";
 AntConfig.StorageLoginPassName = "loginpass";
