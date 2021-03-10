74e4bcbd0d30d9067bbfce3e6cd5a392d98803d3
917388970@qq.com <917388970@qq.com>
2021-03-10 10:35:52 +0800

    更改shell脚本，启弃用GitHub

--- sync_zip_from_github.sh
+++ sync_zip_from_github.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+git fetch --all
+git reset --hard origin/release
+git pull origin/release
+echo "git update successfully"
+
+
 file_dir=/usr/local/jpress/webapp/attachment/
 file_name=auto-js-ant-release.zip
 
 if [[ -e "${file_dir}${file_name}" ]]
 then 
+    echo "backup file successfully"
     mv -f "${file_dir}${file_name}" "${file_dir}${file_name}.bak"
 fi
 
-wget  https://github.com/Lvbey/auto-js-ant/archive/release.zip -O "${file_dir}${file_name}"
+
+git archive --format=zip --output="${file_dir}${file_name}" --prefix="auto-js-ant/" release
+
+
+if [[ -s "${file_dir}${file_name}" ]]
+then
+    echo "synchronized successfully"
+else
+    echo "synchronized failed:the file ${file_name}'s " size is 0
+fi
+
+
+#wget --timeout=30 --waitretry=2 --tries=3  https://github.com/Lvbey/auto-js-ant/archive/release.zip --no-check-certificate -O "${file_dir}${file_name}.tmp"
+
+# if [[ -s "${file_dir}${file_name}.tmp" ]]
+# then
+    # mv "${file_dir}${file_name}.tmp" "${file_dir}${file_name}"
+# else
+    # echo "${file_dir}${file_name}.tmp" size is 0
+# fi
+
+
+
+
+
+
